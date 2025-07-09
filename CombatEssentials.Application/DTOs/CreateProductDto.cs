d6ef78a34413e15bd309370789ceecdc62690664
Qnkisa <yanislav.angelov04@gmail.com>
2025-07-09 18:48:43 +0300

    product dtos created

---
+++ CombatEssentials.Application/DTOs/CreateProductDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace CombatEssentials.Application.DTOs
+{
+    public class CreateProductDto
+    {
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Required]
+        public IFormFile ImageFile { get; set; } = null!;
+    }
+}
---
+++ CombatEssentials.Application/DTOs/UpdateProductDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace CombatEssentials.Application.DTOs
+{
+    public class UpdateProductDto
+    {
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public IFormFile ImageFile { get; set; }
+    }
+}
