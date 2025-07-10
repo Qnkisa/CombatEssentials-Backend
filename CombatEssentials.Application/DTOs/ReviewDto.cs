6bfbe84e9568a03e89e8454ba4c478f6a25d1e8a
Qnkisa <yanislav.angelov04@gmail.com>
2025-07-10 21:56:26 +0300

    added dtos

---
+++ CombatEssentials.Application/DTOs/ReviewDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CombatEssentials.Application.DTOs
+{
+    public class ReviewDto
+    {
+        public int ProductId { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
