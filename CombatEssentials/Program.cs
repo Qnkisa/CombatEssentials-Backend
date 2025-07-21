9ec85d57c50e5dd39f5a25f956accab77b09441f
Yanislav Angelov <yanislav.angelov04@gmail.com>
2025-07-21 16:05:25 +0300

    port changed

--- CombatEssentials/Program.cs
+++ CombatEssentials/Program.cs
@@ -50,7 +50,7 @@ namespace CombatEssentials.API
             {
                 options.AddPolicy("AllowFrontend", policy =>
                 {
-                    policy.WithOrigins("https://localhost:5173")
+                    policy.WithOrigins("http://localhost:3000")
                           .AllowAnyHeader()
                           .AllowAnyMethod()
                           .AllowCredentials();
